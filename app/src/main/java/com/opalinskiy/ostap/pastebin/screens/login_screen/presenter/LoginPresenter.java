5b4e909990ab46e615b4d2ffcf6429d7994a8d17
Ostap Opalinskiy <ostap.opal@gmail.com>
2016-07-04 11:04:19 +0300

    LoginPresenter added

---
+++ app/src/main/java/com/opalinskiy/ostap/pastebin/screens/login_screen/presenter/LoginPresenter.java
@@ -0,0 +1,22 @@
+package com.opalinskiy.ostap.pastebin.screens.login_screen.presenter;
+
+
+import android.content.SharedPreferences;
+
+import com.opalinskiy.ostap.pastebin.screens.login_screen.ILoginScreen;
+import com.opalinskiy.ostap.pastebin.screens.main_screen.IMainScreen;
+import com.opalinskiy.ostap.pastebin.screens.main_screen.presenter.MainScreenPresenter;
+
+
+public class LoginPresenter implements ILoginScreen.IPresenter {
+    IMainScreen.IPresenter mainPresenter;
+
+    public LoginPresenter(IMainScreen.IView view) {
+        mainPresenter = MainScreenPresenter.getInstance(view);
+    }
+
+    @Override
+    public void onLogin(String login, String password, SharedPreferences preferences) {
+        mainPresenter.onLogin(login, password, preferences);
+    }
+}
